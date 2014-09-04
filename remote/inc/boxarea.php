7cdd6251a6fb08adc179e542c4606fd4381a9ec6
Bachmma1 <bachmma1@gmail.com>
2014-09-04 13:01:12 +0200

    Adjust Shoutbox
    inc/boxarea.php:
    - give shoutbox table data an id --> username_shoutbox
    
    styles/onelineblue/style.css:
    - set the nowrap only for username in the shoutbox. Text can be wrapped

--- remote/inc/boxarea.php
+++ remote/inc/boxarea.php
@@ -60,7 +60,7 @@ class Shoutbox
 
 		$shouts = '<table>';
 		while($h = $db->fetch($result))
-			$shouts .= sprintf('<tr><td><strong>%s</strong> <span class="hint">%s</span></td><td>%s</td><td>%s</td></tr>',
+			$shouts .= sprintf('<tr><td id="username_shouts"><strong>%s</strong> <span class="hint">%s</span></td><td>%s</td><td>%s</td></tr>',
 				$db->out($h['name']),
 				date('d.m.y H:i', $h['time']),
 				$this->processMessage($db->out($h['message'])),
